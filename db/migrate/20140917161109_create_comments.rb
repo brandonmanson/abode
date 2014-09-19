949be9e75c6d8e4c07075b4f413d5544e28e2900
Helin Shiah & Kyle Daugherty <pair+helin24.kyledaugherty@devbootcamp.com>
2014-09-19 15:49:39 -0500

    change type to commentable_type

--- db/migrate/20140917161109_create_comments.rb
+++ db/migrate/20140917161109_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.integer :user_id
-      t.string :type
+      t.string :commentable_type
       t.integer :commentable_id
       t.text :description
 
