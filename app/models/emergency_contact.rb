834d3198ed23ae1b32ea112388bf9dbb704a6c69
Helin Shiah <helin.shiah@gmail.com>
2014-09-20 10:19:49 -0500

    new emergency contact and editing emergency contact links work delete causing problems

--- app/models/emergency_contact.rb
+++ app/models/emergency_contact.rb
@@ -1,5 +1,6 @@
 class EmergencyContact < ActiveRecord::Base
   belongs_to :user
   validates :name, :phone, :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, format: { with: VALID_EMAIL_REGEX }
 end
