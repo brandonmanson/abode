38cc19060c7ecf96a432741fa557de7ce1f39d0f
Helin Shiah & Kyle Daugherty <pair+helin24.kyledaugherty@devbootcamp.com>
2014-09-20 11:33:26 -0500

    add comments to expenses

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,14 +4,20 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @dwelling = Dwelling.find(params[:abode_id])
-    comment = @dwelling.comments.new(comment_params)
+    if request.original_url.include?("abodes")
+      commentable = Dwelling.find(params[:abode_id])
+    elsif request.original_url.include?("expenses")
+      commentable = Expense.find(params[:expense_id])
+    end
+    comment = commentable.comments.new(comment_params)
     comment.assign_attributes(user_id: current_user.id)
     if comment.save
-      redirect_to abode_path(params[:abode_id])
+      redirect_to abode_path(params[:abode_id]) if commentable.is_a?(Dwelling)
+      redirect_to expense_path(params[:expense_id]) if commentable.is_a?(Expense)
     else
       flash[:error] = "Description required"
-      redirect_to abode_path(params[:abode_id])
+      redirect_to abode_path(params[:abode_id]) if commentable.is_a?(Dwelling)
+      redirect_to expense_path(params[:expense_id]) if commentable.is_a?(Expense)
     end
   end
 
