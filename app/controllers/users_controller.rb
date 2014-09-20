b27423a71c64a7ee1a0a2f54c0162f6bb68a4873
Brandon Manson & Helin Shiah <pair+brandonmanson.helin24@devbootcamp.com>
2014-09-19 20:12:33 -0500

    trying out mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.welcome_email(@user).deliver
       sign_in(@user)
       redirect_to user_show_path(@user.id)
     else
