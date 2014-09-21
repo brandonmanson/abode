9acfa1a8e8e577e7c1992225da92dca429f39a44
Kyle Daugherty <kyle.daugherty@gmail.com>
2014-09-21 12:12:12 -0500

    remove commented out code

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,20 +1,5 @@
 var app = angular.module('abode', ['ngResource', 'ngRoute']);
 
-// app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-//   $routeProvider.
-//   when('/abodes/:id/comments', {
-//     controller: 'CommentsCtrl',
-//     template: ' '
-//   });
-
-//   $locationProvider.html5Mode(true);
-//   $locationProvider.hashPrefix('!');
-// }]);
-
-// app.factory('Comment', ['$resource', function($resource) {
-//   $resource('/abodes/:abodeId/comments', {abodeId: '@abodeId'});
-// }]);
-
 app.factory('Comment', ['$resource', '$location', function($resource, $location) {
   var url = $location.$$absUrl;
   var commentableId = url.substr(url.lastIndexOf('/') + 1);
@@ -23,17 +8,6 @@ app.factory('Comment', ['$resource', '$location', function($resource, $location)
     isArray:true}
   });
 }]);
-
-
-// app.controller('CommentsCtrl', ['$scope', '$resource', '$routeParams', function($scope, $resource) {
-//   var Comments = $resource('/abodes/' + $routeParams.abodeId + '/comments');
-//   $scope.comments = Comments.query();
-// }]);
-
-// app.controller('CommentsCtrl', ['$scope', '$resource', '$routeParams', function($scope, $resource) {
-//   var Comments = $resource('/abodes/1/comments');
-//   $scope.comments = Comments.query();
-// }]);
 
 app.controller('CommentsCtrl', ['$scope', '$location', 'Comment', function($scope, $location, Comment) {
   $scope.comments = Comment.query();
