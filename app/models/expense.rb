0d4a42f97a6cc39b64012e2493096ffd57a6ea36
Helin Shiah <helin.shiah@gmail.com>
2014-09-21 16:02:01 -0500

    removing unused expenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -25,31 +25,9 @@ class Expense < ActiveRecord::Base
     end
   end
 
-  # def distribute_owed_amounts
-  #   users = self.dwelling.users
-  #   num_users = users.count
-  #   users.each do |u|
-  #     if u.user_expenses.where(expense_id: self.id).length > 0
-  #       u.user_expenses.find_by(expense_id: self.id).update(portion: self.amount / num_users )
-  #     else
-  #       u.user_expenses.create(expense_id: self.id, portion: self.amount / num_users)
-  #     end
-  #   end
-  # end
-
   def amount_must_be_distributed
     if self.amount != self.user_expenses.reduce(0){|sum, e| sum = sum + e.portion}
       errors.add(:amount, "must be distributed fully among users sharing the expense")
-    end
-  end
-
-  def redistribute_owed_amounts
-    old_user_expense_total = self.user_expenses.sum("portion")
-    if old_user_expense_total != self.amount
-      self.user_expenses.each do |user_expense|
-        user_expense.portion = (user_expense.portion/old_user_expense_total) * self.amount
-        user_expense.save
-      end
     end
   end
 
