ba8ecf792ea154f470e2f0e3707dc681ac07c777
Kyle Daugherty <kyle.daugherty@gmail.com>
2014-09-21 12:41:52 -0500

    add angularjs save function for new comments

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -4,11 +4,18 @@ app.factory('Comment', ['$resource', '$location', function($resource, $location)
   var url = $location.$$absUrl;
   var commentableId = url.substr(url.lastIndexOf('/') + 1);
   return $resource('/abodes/:id/comments', {}, {
-    query: { method: 'GET', params: {id: commentableId},
-    isArray:true}
+    query: {method: 'GET', params: {id: commentableId},
+    isArray:true},
+    save: {method: 'POST', params: {id: commentableId}}
   });
 }]);
 
 app.controller('CommentsCtrl', ['$scope', '$location', 'Comment', function($scope, $location, Comment) {
   $scope.comments = Comment.query();
+
+  $scope.addComment = function() {
+    comment = Comment.save($scope.newComment);
+    $scope.comments.push(comment);
+    $scope.newComment = {};
+  };
 }]);
